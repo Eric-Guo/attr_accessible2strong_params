f06eadbf0117002952fb0f93ae05adab60ccfd5c
Eric Guo <eric.guo@sandisk.com>
2014-10-03 12:47:44 +0800

    Extract private methods parse_file_with_comments

--- lib/attr_accessible2strong_params/converter.rb
+++ lib/attr_accessible2strong_params/converter.rb
@@ -4,26 +4,28 @@ require 'unparser'
 
 class AttrAccessible2StrongParams::Converter
   def read_attr_accessible(filename)
-    buffer = Parser::Source::Buffer.new(filename)
-    buffer.source = File.read(filename)
-
-    builder = Astrolabe::Builder.new
-    parser = Parser::CurrentRuby.new(builder)
-
-    root_node = parser.parse(buffer)
+    root_node, comments = parse_file_with_comments(filename)
     m = root_node.each_node(:send).select {|n| n.children[1] == :attr_accessible}.first
     @model_class_name = m.parent.parent.children[0].children[1]
     @aa_list = m.each_node(:sym).collect {|n| n.children[0]}
   end
 
   def write_controller_with_strong_params(filename)
-    ast, comments = Parser::CurrentRuby.parse_with_comments(File.read(filename))
-    rewritten = Unparser.unparse(ast, comments)
+    root_node, comments = parse_file_with_comments(filename)
+    rewritten = Unparser.unparse(root_node, comments)
     temp_path = "#{filename}.rewritten"
     File.open(temp_path, 'w') do |temp_file|
       temp_file.write(rewritten)
       temp_file.puts unless rewritten.end_with?(?\n)
     end
     File.rename(temp_path, filename)
+  end
+
+private
+  def parse_file_with_comments(filename)
+    parser = Parser::CurrentRuby.new(Astrolabe::Builder.new)
+    buffer = Parser::Source::Buffer.new(filename)
+    buffer.source = File.read(filename)
+    parser.parse_with_comments(buffer)
   end
 end
